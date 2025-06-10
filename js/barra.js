150183dc0351b0930f10ee8b58ec859149828070
PieroSanchez <pierosancas@gmail.com>
2025-06-10 01:08:25 -0500

    ultimo commit del dia

---
+++ js/barra.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const counters = document.querySelectorAll('.counter');
+    const speed = 100; 
+
+    const animateCounters = () => {
+      counters.forEach(counter => {
+        const updateCount = () => {
+          const target = +counter.getAttribute('data-target');
+          const count = +counter.innerText.replace(/,/g, '');
+
+        
+          const increment = Math.max(Math.floor(target / speed), 1);
+
+          if (count < target) {
+            counter.innerText = formatNumber(count + increment);
+            setTimeout(updateCount, 20);
+          } else {
+            counter.innerText = formatNumber(target);
+          }
+        };
+
+        updateCount();
+      });
+    };
+
+    const formatNumber = (num) => {
+      return num.toLocaleString('en-US'); 
+    };
+
+   
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          animateCounters();
+          observer.disconnect();
+        }
+      });
+    }, { threshold: 0.3 });
+
+    if (counters.length) observer.observe(counters[0]);
+  });
+
